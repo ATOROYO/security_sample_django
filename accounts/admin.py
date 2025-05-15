be8fe02c15f797c9da87f02337623a998dfdabfb
Atoroyo <32022316+ATOROYO@users.noreply.github.com>
2025-05-15 23:45:41 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin😍
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
