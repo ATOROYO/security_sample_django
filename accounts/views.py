93b25bc939a5502148165a2de6cdad9333ce230e
Atoroyo <32022316+ATOROYO@users.noreply.github.com>
2025-09-23 23:59:33 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-😊6from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth.decorators import login_required
